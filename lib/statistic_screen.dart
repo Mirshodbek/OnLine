40ed630a7ccfbf0249f6c4fe21013f05a33ad41a
Mirshodbek <mirshod.bahromov@mail.ru>
2020-12-04 17:47:09 +0500

    added list_of_screen and statistic_screen

---
+++ lib/statistic_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StatisticScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
