b316aea295e2efd103c15620d6410d1814d44c4d
Mirshodbek <mirshod.bahromov@mail.ru>
2020-12-04 17:42:41 +0500

    set buttons in home_screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,13 +2,41 @@ import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   static final String id = 'home_screen';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text('OnLine'),
       ),
-      body: Container(),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/background.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RaisedButton(
+                onPressed: () {},
+                child: Text('Stand in line'),
+              ),
+              RaisedButton(
+                onPressed: () {},
+                child: Text("List of visitors"),
+              ),
+              RaisedButton(
+                onPressed: () {},
+                child: Text('Statistics'),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
